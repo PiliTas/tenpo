9412f0d107690a7359e4c66278d11ef762a4dbb9
Ieva <55699770+ievastumpe@users.noreply.github.com>
2020-05-19 20:56:49 -0400

    Revert "structure of register page (html), and bit of scss"

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,16 +6,7 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  },
-  {
-    path: 'inforegister',
-    loadChildren: () => import('../inforegister/inforegister.module').then( m => m.InforegisterPageModule)
-  },
-
-  {
-    path: 'sesion',
-    loadChildren: () => import('../sesion/sesion.module').then( m => m.SesionPageModule)
-  },
+  }
 ];
 
 @NgModule({
